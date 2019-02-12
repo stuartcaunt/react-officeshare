913b326920d1af4faed8d438047168d534430257
Jamie Hall <hall@ill.eu>
2019-02-12 20:26:10 +0100

    Modularising

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 export class Header extends Component<{ participants: number, title: string, toggleChat: () => void }, {}> {
-    
+
     render() {
         const {title, participants} = this.props;
         return <div className="header">
             <div className="left">
-                <i className="fas fa-users"/><span>{participants} participants</span>
+                <span>https://jamie.me/jfd4lksl</span>
+                <button className="copy-link">Copy link</button>
+                {/*<i className="fas fa-users"/><span>{participants} participants</span>*/}
             </div>
             <div className="center">{title}</div>
             <div className="right">
